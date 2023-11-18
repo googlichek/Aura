ad506396f170707fe3294222122f24574f2f9472
Goga Akobiya <googlichek.one@gmail.com>
2023-11-18 14:42:30 +0400

    Switching to the custom context.

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright Turtle Shell Games
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemGlobals.h
@@ -0,0 +1,18 @@
+﻿// Copyright Turtle Shell Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemGlobals.h"
+#include "AuraAbilitySystemGlobals.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemGlobals : public UAbilitySystemGlobals
+{
+	GENERATED_BODY()
+
+	virtual FGameplayEffectContext* AllocGameplayEffectContext() const override;
+};
